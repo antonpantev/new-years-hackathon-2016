cec03d4d9dfca772eda76bc54d03647a2ae01db2
Anton Pantev <pantevanton@gmail.com>
2016-01-01 10:04:29 -0800

    randomizing the unplaced tiles

--- app/js/index.js
+++ app/js/index.js
@@ -9,11 +9,19 @@ function createTiles(minColor, maxColor, numTiles) {
 	/* Add unplaced tiles */
 	var incr = Math.round((maxColor - minColor) / (numTiles - 1));
 
+	var colors = [];
+
 	for(var i = 0; i < (numTiles - 1); i++) {
-		addUnplacedTile(minColor + incr*i);
+		colors[i] = minColor + incr*i;
 	}
 	
-	addUnplacedTile(maxColor);
+	colors[i] = maxColor;
+
+	shuffle(colors);
+
+	for(var i = 0; i < colors.length; i++) {
+		addUnplacedTile(colors[i]);
+	}
 
 	/* Add empty placed tiles */
 	$('#placed-tiles').append('<hr class="placed-tile-divider">');
@@ -21,6 +29,25 @@ function createTiles(minColor, maxColor, numTiles) {
 	for(var i = 0; i < numTiles; i++) {
 		addEmptyPlacedTile(i+1);
 	}
+}
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  /* While there remain elements to shuffle */
+  while (0 !== currentIndex) {
+
+    /* Pick a remaining element */
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    /* And swap it with the current element. */
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
 
 function addUnplacedTile(color) {
