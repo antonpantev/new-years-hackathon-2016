be7615974787a7818a0079a89793c60318629a10
Anton Pantev <pantevanton@gmail.com>
2016-01-01 11:25:32 -0800

    simplifying the app. adding a logo.

--- app/js/index.js
+++ app/js/index.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
-	createTiles(0x99, 0xf7, 94);
+	createTiles(0x99, 0xf7, 14);
 
-	$(window).resize(resizeUnplacedTile);
-	resizeUnplacedTile();
+	$(window).resize(resizeTiles);
+	resizeTiles();
 });
 
 function createTiles(minColor, maxColor, numTiles) {
-	/* Add unplaced tiles */
 	var incr = Math.round((maxColor - minColor) / (numTiles - 1));
 
 	var colors = [];
@@ -20,57 +19,39 @@ function createTiles(minColor, maxColor, numTiles) {
 	shuffle(colors);
 
 	for(var i = 0; i < colors.length; i++) {
-		addUnplacedTile(colors[i]);
-	}
-
-	/* Add empty placed tiles */
-	$('#placed-tiles').append('<hr class="placed-tile-divider">');
-
-	for(var i = 0; i < numTiles; i++) {
-		addEmptyPlacedTile(i+1);
+		addTile(colors[i]);
 	}
 }
 
 function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+	var currentIndex = array.length, temporaryValue, randomIndex;
 
-  /* While there remain elements to shuffle */
-  while (0 !== currentIndex) {
+	/* While there remain elements to shuffle */
+	while (0 !== currentIndex) {
+		/* Pick a remaining element */
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
 
-    /* Pick a remaining element */
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+		/* And swap it with the current element. */
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
 
-    /* And swap it with the current element. */
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
+	return array;
 }
 
-function addUnplacedTile(color) {
+function addTile(color) {
 	var colorStr = "#" + color.toString(16) + color.toString(16) + color.toString(16);
 
-	var $unplacedTile = $('<div class="unplaced-tile"></div>');
-	$unplacedTile.css("background-color", colorStr);
+	var $tile = $('<div class="tile"></div>');
+	$tile.css("background-color", colorStr);
 
-	$('#unplaced-tiles').append($unplacedTile);
+	$('#tiles').append($tile);
 }
 
-function addEmptyPlacedTile(num) {
-	$('#placed-tiles').append('<div class="placed-tile">' + zeroPad(num, 2) + '</div>');
-	$('#placed-tiles').append('<hr class="placed-tile-divider">');
-}
-
-function zeroPad(n, p) {
-	var pad = new Array(1 + p).join('0');
-	return (pad + n).slice(-pad.length);
-}
-
-function resizeUnplacedTile() {
-	var $tiles = $('.unplaced-tile');
+function resizeTiles() {
+	var $tiles = $('.tile');
 
 	var width = $tiles.width();
 	$tiles.css({'height': width + 'px'});
