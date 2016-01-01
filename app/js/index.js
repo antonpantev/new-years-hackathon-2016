337cddf584d357264bbc77275ad7b50f9c116214
Anton Pantev <pantevanton@gmail.com>
2015-12-31 22:15:55 -0800

    beginning to create the unplaced tiles

---
+++ app/js/index.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	createTiles(0x99, 0xee, 6);
+
+	$(window).resize(resizeUnplacedTile);
+	resizeUnplacedTile();
+});
+
+function createTiles(minColor, maxColor, numTiles) {
+	var incr = Math.round((maxColor - minColor) / (numTiles - 1));
+
+	for(var i = 0; i < (numTiles - 1); i++) {
+		addTile(minColor + incr*i);
+	}
+	
+	addTile(maxColor);
+}
+
+function addTile(color) {
+	var colorStr = "#" + color.toString(16) + color.toString(16) + color.toString(16);
+
+	var $tile = $('<div class="unplaced-tile"></div>');
+	$tile.css("background-color", colorStr);
+
+	$('#unplaced-tiles').append($tile);
+}
+
+function resizeUnplacedTile() {
+	var $tiles = $('.unplaced-tile');
+
+	var width = $tiles.width();
+	$tiles.css({'height': width + 'px'});
+}
